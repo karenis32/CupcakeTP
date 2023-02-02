5aab84325241ea9ab946833f6874b00f743cb761
karenis32 <karen.is32@hotmail.com>
2023-02-02 12:36:37 -0300

    TP terminado

---
+++ Cupcakes/Startup.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Cupcakes.Data;
+using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Http;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using Cupcakes.Repositories;
+
+namespace Cupcakes
+{
+    public class Startup
+    {
+        public void ConfigureServices(IServiceCollection services)
+        {
+            services.AddTransient<ICupcakeRepository, CupcakeRepository>();
+
+            services.AddDbContext<CupcakeContext>(options =>
+                 options.UseSqlServer(_configuration.GetConnectionString("DefaultConnection")));
+            services.AddMvc();
+        }
+
+        private IConfiguration _configuration;
+
+        public Startup(IConfiguration configuration)
+        {
+            _configuration = configuration;
+        }
+
+        public void Configure(IApplicationBuilder app)
+        {
+
+            app.UseStaticFiles();
+
+            app.UseMvc(routes =>
+            {
+                routes.MapRoute(
+                    name: "CupcakeRoute",
+                    template: "{controller}/{action}/{id?}",
+                    defaults: new { controller = "Cupcake", action = "Index" },
+                    constraints: new { id = "[0-9]+" });
+            });
+        }
+    }
+}
